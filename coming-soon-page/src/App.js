0a0f798ef0b1de459e4e8a3192fe317e28adb62d
SOUFIANEMEJAHED <soufiane.mejahed@epitech.eu>
2023-06-30 22:35:39 +0200

    recat projetc

---
+++ coming-soon-page/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ComingSoon from "./ComingSoon";
+// Import the functions you need from the SDKs you need
+
+function App() {
+  return (
+    <div className="App">
+      <ComingSoon />
+    </div>
+  );
+}
+
+export default App;
---
+++ coming-soon-page/src/ComingSoon.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./ComingSoon.css";
+
+const ComingSoon = () => {
+  return (
+    <div className="coming-soon">
+      <h1>Coming Soon</h1>
+      <p>We are working hard to bring you an amazing website. Stay tuned!</p>
+    </div>
+  );
+};
+
+export default ComingSoon;
